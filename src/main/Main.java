6c34bd50527336f3c4c8f9c184c9873fb6808d42
Jago128 <jagobaetxegarai@gmail.com>
2025-02-24 13:14:46 +0100

    e

--- src/main/Main.java
+++ src/main/Main.java
@@ -1,8 +1,11 @@
 package main;
 
+import windows.Window1;
+
 public class Main {
 
 	public static void main(String[] args) {
-		
+		Window1 frame = new Window1();
+		frame.setVisible(true);
 	}
 }
--- src/windows/Window1.java
+++ src/windows/Window1.java
@@ -1,40 +1,55 @@
 package windows;
 
-import java.awt.*;
+import java.awt.event.*;
+
 import javax.swing.*;
 import javax.swing.border.*;
 
-public class Window1 extends JFrame {
+public class Window1 extends JFrame implements ActionListener {
 
 	private static final long serialVersionUID = 1L;
 	private JPanel contentPane;
-
-	/**
-	 * Launch the application.
-	 */
-	public static void main(String[] args) {
-		EventQueue.invokeLater(new Runnable() {
-			public void run() {
-				try {
-					Window1 frame = new Window1();
-					frame.setVisible(true);
-				} catch (Exception e) {
-					e.printStackTrace();
-				}
-			}
-		});
-	}
-
+	private JTextField textFieldName;
+	private JButton btnContinue;
+	private JButton btnSalir;
+	
 	/**
 	 * Create the frame.
 	 */
 	public Window1() {
 		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-		setBounds(100, 100, 450, 300);
+		setBounds(100, 100, 464, 247);
 		contentPane = new JPanel();
 		contentPane.setBorder(new EmptyBorder(5, 5, 5, 5));
-
 		setContentPane(contentPane);
+		contentPane.setLayout(null);
+		
+		JLabel lblName = new JLabel("Introduce tu nombre o apodo:");
+		lblName.setBounds(140, 38, 153, 35);
+		contentPane.add(lblName);
+		
+		textFieldName = new JTextField();
+		textFieldName.setBounds(116, 73, 197, 28);
+		contentPane.add(textFieldName);
+		textFieldName.setColumns(10);
+		
+		btnContinue = new JButton("Siguiente");
+		btnContinue.setBounds(116, 120, 89, 23);
+		contentPane.add(btnContinue);
+		btnContinue.addActionListener(this);
+		
+		btnSalir = new JButton("Salir");
+		btnSalir.setBounds(225, 120, 89, 23);
+		contentPane.add(btnSalir);
+		btnSalir.addActionListener(this);
 	}
 
+	@Override
+	public void actionPerformed(ActionEvent e) {
+		if (e.getSource()==btnSalir) {
+			dispose();
+		} else {
+			
+		}
+	}
 }
